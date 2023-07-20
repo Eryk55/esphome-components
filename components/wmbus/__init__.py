88b73b06c570478dcd1b8948328aa155d6050357
Eryk55 <125482895+Eryk55@users.noreply.github.com>
2023-07-20 14:11:46 +0200

    C1

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -41,6 +41,25 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.polling_component_schema("60s"))
 
 
+
+
+def my_key(value):
+    value = cv.string_strict(value)
+    parts = [value[i : i + 2] for i in range(0, len(value), 2)]
+    if (len(parts) != 16) and (len(parts) != 0):
+        raise cv.Invalid("Key must consist of 16 hexadecimal numbers")
+    parts_int = []
+    if any(len(part) != 2 for part in parts):
+        raise cv.Invalid("Key must be format XX")
+    for part in parts:
+        try:
+            parts_int.append(int(part, 16))
+        except ValueError:
+            # pylint: disable=raise-missing-from
+            raise Invalid("Key must be hex values from 00 to FF")
+    return "".join(f"{part:02X}" for part in parts_int)
+
+
 METER_LISTENER_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_WMBUS_ID): cv.use_id(WMBusComponent),
